ce81c4fbd2a61de2528ff3365ce95c20f9cb4018
Codekaiser03-2003 <achintya.singh.cd.civ22@iitbhu.ac.in>
2024-08-28 01:38:44 +0530

    SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -62,9 +62,7 @@ SET month_name = MONTHNAME(date);
 -- ---------------------------- Generic ------------------------------
 -- --------------------------------------------------------------------
 -- How many unique cities does the data have?
-SELECT 
-	DISTINCT city
-FROM sales;
+SELECT DISTINCT city FROM sales;
 
 -- In which city is each branch?
 SELECT 
