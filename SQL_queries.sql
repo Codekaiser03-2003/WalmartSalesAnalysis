96018e92a43a88823371647755e12d2fbd71a86c
Codekaiser03-2003 <achintya.singh.cd.civ22@iitbhu.ac.in>
2024-08-28 01:14:21 +0530

    SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -24,27 +24,15 @@ CREATE TABLE IF NOT EXISTS sales(
 );
 
 -- Data cleaning
-SELECT
-	*
-FROM sales;
+SELECT * FROM sales;
 
 
 -- Add the time_of_day column
-SELECT
-	time,
-	(CASE
-		WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
-        WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
-        ELSE "Evening"
-    END) AS time_of_day
-FROM sales;
-
 
 ALTER TABLE sales ADD COLUMN time_of_day VARCHAR(20);
 
 -- For this to work turn off safe mode for update
--- Edit > Preferences > SQL Edito > scroll down and toggle safe mode
--- Reconnect to MySQL: Query > Reconnect to server
+set sql_safe_updates = 0;
 UPDATE sales
 SET time_of_day = (
 	CASE
