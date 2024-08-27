9a78d3327776aa24ac76f512a71855612d648b8d
Codekaiser03-2003 <achintya.singh.cd.civ22@iitbhu.ac.in>
2024-08-28 01:46:47 +0530

    SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -76,61 +76,49 @@ FROM sales;
 SELECT DISTINCT product_line FROM sales;
 
 -- What is the most selling product line
-SELECT
-	SUM(quantity) as qty,
-    product_line
+SELECT SUM(quantity) as qty, product_line
 FROM sales
 GROUP BY product_line
 ORDER BY qty DESC;
 
 -- What is the most selling product line
-SELECT
-	SUM(quantity) as qty,
-    product_line
+SELECT SUM(quantity) as qty, product_line
 FROM sales
 GROUP BY product_line
 ORDER BY qty DESC;
 
 -- What is the total revenue by month
-SELECT
-	month_name AS month,
-	SUM(total) AS total_revenue
+SELECT month_name AS month,
+SUM(total) AS total_revenue
 FROM sales
 GROUP BY month_name 
 ORDER BY total_revenue;
 
 
 -- What month had the largest COGS?
-SELECT
-	month_name AS month,
-	SUM(cogs) AS cogs
+SELECT month_name AS month,
+SUM(cogs) AS cogs
 FROM sales
 GROUP BY month_name 
 ORDER BY cogs;
 
 
 -- What product line had the largest revenue?
-SELECT
-	product_line,
-	SUM(total) as total_revenue
+SELECT product_line,
+SUM(total) as total_revenue
 FROM sales
 GROUP BY product_line
 ORDER BY total_revenue DESC;
 
 -- What is the city with the largest revenue?
-SELECT
-	branch,
-	city,
-	SUM(total) AS total_revenue
+SELECT branch, city, SUM(total) AS total_revenue
 FROM sales
 GROUP BY city, branch 
 ORDER BY total_revenue;
 
 
 -- What product line had the largest VAT?
-SELECT
-	product_line,
-	AVG(tax_pct) as avg_tax
+SELECT product_line, AVG(tax_pct) as avg_tax
 FROM sales
 GROUP BY product_line
 ORDER BY avg_tax DESC;
